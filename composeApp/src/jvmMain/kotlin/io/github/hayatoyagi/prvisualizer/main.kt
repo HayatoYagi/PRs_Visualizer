2716fe18b34811345774d9e41504e85aeaeb0d47
HayatoYagi <hayatoy1013@gmail.com>
2026-03-01 19:18:30 +0900

    Set dock icon before Compose application startup

--- composeApp/src/jvmMain/kotlin/io/github/hayatoyagi/prvisualizer/main.kt
+++ composeApp/src/jvmMain/kotlin/io/github/hayatoyagi/prvisualizer/main.kt
@@ -9,25 +9,29 @@ import java.awt.Taskbar
 import java.net.URI
 import javax.imageio.ImageIO
 
-fun main() = application {
+fun main() {
     setDockAndTaskbarIcon()
 
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = "GitHubPRsVisualizer",
-        icon = painterResource(Res.drawable.icon),
-    ) {
-        App()
+    application {
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = "GitHubPRsVisualizer",
+            icon = painterResource(Res.drawable.icon),
+        ) {
+            App()
+        }
     }
 }
 
 private fun setDockAndTaskbarIcon() {
     if (!Taskbar.isTaskbarSupported()) return
+    val taskbar = Taskbar.getTaskbar()
+    if (!taskbar.isSupported(Taskbar.Feature.ICON_IMAGE)) return
 
     runCatching {
         val iconUri = URI.create(Res.getUri("drawable/icon.png")).toURL()
         ImageIO.read(iconUri)?.let { image ->
-            Taskbar.getTaskbar().iconImage = image
+            taskbar.iconImage = image
         }
     }
 }
