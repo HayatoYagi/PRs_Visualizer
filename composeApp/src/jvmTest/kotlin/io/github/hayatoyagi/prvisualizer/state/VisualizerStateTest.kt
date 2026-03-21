68adcb7b5e2a5b248ea8745db4abb118ad90e0ec
HayatoYagi <hayatoy1013@gmail.com>
2026-03-21 13:06:33 +0900

    Move visualizer state test into state package

--- composeApp/src/jvmTest/kotlin/io/github/hayatoyagi/prvisualizer/state/VisualizerStateTest.kt
+++ composeApp/src/jvmTest/kotlin/io/github/hayatoyagi/prvisualizer/state/VisualizerStateTest.kt
@@ -1,18 +1,8 @@
-package io.github.hayatoyagi.prvisualizer
+package io.github.hayatoyagi.prvisualizer.state
 
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.state.ToggleableState
-import io.github.hayatoyagi.prvisualizer.state.AuthState
-import io.github.hayatoyagi.prvisualizer.state.ColorState
-import io.github.hayatoyagi.prvisualizer.state.DialogState
-import io.github.hayatoyagi.prvisualizer.state.FilterState
-import io.github.hayatoyagi.prvisualizer.state.NavigationState
-import io.github.hayatoyagi.prvisualizer.state.PrSelection
-import io.github.hayatoyagi.prvisualizer.state.SnapshotFetchState
-import io.github.hayatoyagi.prvisualizer.state.VisualizerState
-import io.github.hayatoyagi.prvisualizer.state.resetForRepositoryChange
-import io.github.hayatoyagi.prvisualizer.state.resetNavigation
-import io.github.hayatoyagi.prvisualizer.state.resetViewport
+import io.github.hayatoyagi.prvisualizer.AppError
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
